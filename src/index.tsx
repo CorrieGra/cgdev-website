d9abacd68322b6db5db41881e97213d8b266507d
Corrie C. Graham <corrieg@ornico.co.za>
2021-08-13 16:46:59 +0200

    chore: wrapped app with store provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,20 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import store from './store';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { StoreProvider } from 'easy-peasy';
 
 const _history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={ _history }>
-      <App />
-    </Router>
+    <StoreProvider store={ store }>
+      <Router history={ _history }>
+        <App />
+      </Router>
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
