16ff3809dc44299a0060b21db3341e2171d0981a
Corrie C. Graham <corrieg@ornico.co.za>
2021-08-13 20:56:55 +0200

    chore: added data prop

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,29 +1,8 @@
 import './Timeline.css';
-import React, { useState } from 'react';
+import React from 'react';
 import moment from 'moment';
 
-export function Timeline(): JSX.Element {
-    const [items] = useState([
-        {
-            experience_company: "Cenoex",
-            experience_company_location: "Pretoria, Gauteng",
-            experience_start_date: "2018-02-01",
-            experience_end_date: "2021-01-31",
-            experience_is_ongoing: false,
-            experience_position: "Junior Software Developer",
-            experience_technologies: ["html5", "css3", "javascript", "php", "c#", "sql"]
-        },
-        {
-            experience_company: "Ornico",
-            experience_company_location: "Johannesburg, Gauteng",
-            experience_start_date: "2021-01-18",
-            experience_end_date: null,
-            experience_is_ongoing: true,
-            experience_position: "Intermediate Software Developer ( React )",
-            experience_technologies: ["react", "redux", "react router", "typescript", "axios", "scss", "css3"]
-        }
-    ]);
-
+export function Timeline(props: any): JSX.Element {
     const calculateExperienceDuration = (startDate: string | null, endDate: string | null): string => {
         let result: string;
         
@@ -53,13 +32,13 @@ export function Timeline(): JSX.Element {
     return (
         <div className="timeline">
             {
-                items.map((item, index) => (
+                props.data.map((item: any, index: number) => (
                     <div className="timeline__item" key={ index }>
                         <div className="timeline__item--title">
                             <h3>{ item.experience_position }, { item.experience_company } , { moment(item.experience_start_date).format('MMM YYYY') } - { item.experience_is_ongoing ? 'Present' : moment(item.experience_end_date).format('MMM YYYY') }</h3>
                         </div>
                         <div className="timeline__item--duration">
-                            <span>{ item.experience_is_ongoing ? calculateExperienceDuration(item.experience_start_date, new Date().toLocaleString() ) : calculateExperienceDuration(item.experience_start_date, item.experience_end_date) }</span>
+                            <span>{ item.experience_is_ongoing ? calculateExperienceDuration(item.experience_start_date, new Date().toISOString() ) : calculateExperienceDuration(item.experience_start_date, item.experience_end_date) }</span>
                         </div>
                         <div className="timeline__item--company">
                             <span>{ item.experience_company }</span>
@@ -69,7 +48,7 @@ export function Timeline(): JSX.Element {
                         </div>
                         <div className="timeline__item--technologies">
                             {
-                                item.experience_technologies.map((technology, index) => (
+                                item.experience_technologies.map((technology: string, index: number) => (
                                     <span key={ index } className="timeline__item--technology">{ technology }</span>
                                 ))
                             }
