d2676ac733e81c483f5b203a72bb733280ce909e
Corrie C. Graham <corrieg@ornico.co.za>
2021-08-13 12:55:10 +0200

    chore: changed fill-rule to fillRule

--- src/components/Utils/Button/Button.tsx
+++ src/components/Utils/Button/Button.tsx
@@ -16,7 +16,7 @@ export function Button(props: IButtonParams): JSX.Element {
     const PrimaryButton = () => (
         <button className={ `btn btn--${props.type}` } onClick={ props.onClick } disabled={ props.disabled !== undefined ? props.disabled : false }>
             <div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="14"><g fill="none" fill-rule="evenodd" stroke="#5FB4A2"><path d="M0 9l8 4 8-4"/><path opacity=".5" d="M0 5l8 4 8-4"/><path opacity=".25" d="M0 1l8 4 8-4"/></g></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="14"><g fill="none" fillRule="evenodd" stroke="#5FB4A2"><path d="M0 9l8 4 8-4"/><path opacity=".5" d="M0 5l8 4 8-4"/><path opacity=".25" d="M0 1l8 4 8-4"/></g></svg>
             </div>
             <div>{ props.text }</div>
         </button>
