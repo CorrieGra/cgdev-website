995d1e03d904c68f6321beb8b0e80087118506a2
Corrie C. Graham <corrieg@ornico.co.za>
2021-08-13 12:11:21 +0200

    chore: added links around about me call to actions

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import { Section } from "../../components/Layout/Section/Section";
 import { Timeline } from "../../components/Timeline/Timeline";
 import { Button } from "../../components/Utils/Button/Button";
@@ -16,8 +17,12 @@ export function HomeView() {
                     Experienced in full-stack development, with a passion and focus on front-end development. I started my career with an amazing opportunity, after graduating from matric and have grown through all of my experiences and thanks to the help of my seniors. I'm passionate and enjoy the line of work I'm in, always looking to improve and to learn new skills. I work great in teams and I'm perfectly capable to work on my own, I take responsibilities very seriously and make honesty a priority.
                 </p>
                 <div className="section__cta">
-                    <Button type="secondary" text="go to portfolio" />
-                    <Button type="secondary" text="download resume" />
+                    <Link to="/portfolio">
+                        <Button type="secondary" text="go to portfolio" />
+                    </Link>
+                    <a download href="" rel="noreferrer">
+                        <Button type="secondary" text="download resume" />
+                    </a>
                 </div>
             </Section>
 
