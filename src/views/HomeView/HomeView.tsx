a2e61281fd2433218b7023536914f2ca90b9da65
Corrie C. Graham <corrieg@ornico.co.za>
2021-08-13 22:11:59 +0200

    chore: added id to section for about me

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -24,7 +24,7 @@ export function HomeView() {
             hasIntro
             intro="Hello, I'm Corrie Graham and I love to build beautiful websites"/>
 
-            <Section title="about me">
+            <Section id="about-me" title="about me">
                 <p>
                     Experienced in full-stack development, with a passion and focus on front-end development. I started my career with an amazing opportunity, after graduating from matric and have grown through all of my experiences and thanks to the help of my seniors. I'm passionate and enjoy the line of work I'm in, always looking to improve and to learn new skills. I work great in teams and I'm perfectly capable to work on my own, I take responsibilities very seriously and make honesty a priority.
                 </p>
