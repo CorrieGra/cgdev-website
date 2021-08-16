1be544be9cef87386b081586bc67dd0dcb17f9e9
Corrie C. Graham <corrieg@ornico.co.za>
2021-08-16 15:23:47 +0200

    refac: removed unnecessary code

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,12 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import Cookies from 'universal-cookie/es6';
 import store from './store';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { StoreProvider } from 'easy-peasy';
 
 const _history = createBrowserHistory();
-
-const cookies = new Cookies();
-cookies.set('jwt', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI4ODcwNjk1LCJleHAiOjE2NjA0OTMwOTV9.JkhI-kpACqv6xZy_Te91CV1cHTvfSKI1jBpSJnmkNsU', { path: '/' })
 
 ReactDOM.render(
   <React.StrictMode>
