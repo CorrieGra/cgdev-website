824a18116e868dfdafc4e8d2ad83831fd07e36e9
Corrie C. Graham <corrieg@ornico.co.za>
2021-08-20 15:34:08 +0200

    feat: implemented notifications

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+// import 'animate.css/animate.min.css';
+import 'react-notifications-component/dist/theme.css'
 import App from './components/App/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactNotification from 'react-notifications-component'
 import store from './store';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
@@ -13,6 +16,7 @@ ReactDOM.render(
   <React.StrictMode>
     <StoreProvider store={ store }>
       <Router history={ _history }>
+        <ReactNotification />
         <App />
       </Router>
     </StoreProvider>
