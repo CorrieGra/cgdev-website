6dabef5e302aa71aad8a765aa12fc18003860c68
Corrie Graham <corriegraham@Corries-MacBook-Air.local>
2021-08-27 15:05:41 +0200

    chore: implemented animations

--- src/components/Layout/Section/Section.tsx
+++ src/components/Layout/Section/Section.tsx
@@ -1,5 +1,6 @@
 import './Section.css';
 import React from 'react';
+import { motion } from 'framer-motion';
 
 interface ISection {
     title?: string;
@@ -8,13 +9,38 @@ interface ISection {
     isVisible?: boolean;
 }
 
+export const sectionVariants = {
+    initial: {
+        opacity: 0,
+        x: 1000
+    },
+    entry: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            delay: .3,
+            duration: .5,
+            when: 'beforeChildren',
+            staggerChildren: .5
+        }
+    },
+    exit: {
+        x: 1000,
+        opacity: 0
+    }
+};
+
 export function Section(props: ISection) {
     return (
-        <section id={ props.id }>
+        <motion.section 
+        variants={ sectionVariants }
+        initial="initial"
+        animate="entry"
+        id={ props.id }>
             <div className="section__header">
                 <h1>{ props.title }</h1>
             </div>
             { props.children }
-        </section>
+        </motion.section>
     );
 };
--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,19 +1,20 @@
 // /* eslint-disable react-hooks/exhaustive-deps */
 import React, { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Section } from "../../components/Layout/Section/Section";
+import { Section, sectionVariants } from "../../components/Layout/Section/Section";
 import { Timeline } from "../../components/Timeline/Timeline";
 import { Button } from "../../components/Utils/Button/Button";
 import { Header } from "../../components/Utils/Header/Header";
 import { createTypedHooks } from 'easy-peasy';
 import { IStore } from "../../store";
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { isMobile } from "react-device-detect";
 
 
 const { useStoreState, useStoreActions } = createTypedHooks<IStore>();
 
 export function HomeView() {
+    const [aboutPositionFromTop, setAboutPositionFromTop] = useState(document.querySelector('#about-me')?.getBoundingClientRect().top);
     const [experiencePositionFromTop, setExperiencePositionFromTop] = useState(document.querySelector('#experience')?.getBoundingClientRect().top);
     const [yPosition, setYPosition] = useState(window.pageYOffset);
     const experiences = useStoreState((state) => state.experiences);
@@ -26,6 +27,7 @@ export function HomeView() {
 
     useEffect(() => {
         setExperiencePositionFromTop(document.querySelector('#experience')?.getBoundingClientRect().top);
+        setAboutPositionFromTop(document.querySelector('#about-me')?.getBoundingClientRect().top);
     }, [yPosition]);
 
     return (
@@ -36,19 +38,31 @@ export function HomeView() {
             hasIntro
             />
 
-            <Section id="about-me" title="about me">
-                <p>
-                    Experienced in full-stack development, with a passion and focus on front-end development. I started my career with an amazing opportunity, after graduating from matric and have grown through all of my experiences and thanks to the help of my seniors. I'm passionate and enjoy the line of work I'm in, always looking to improve and to learn new skills. I work great in teams and I'm perfectly capable to work on my own, I take responsibilities very seriously and make honesty a priority.
-                </p>
-                <div className="section__cta">
-                    <Link to="/portfolio">
-                        <Button type="secondary" text="go to portfolio" />
-                    </Link>
-                    <a download href={ `${process.env.REACT_APP_FILE_URL}/Corrie_Graham-CV.pdf` } rel="noreferrer" target="_blank">
-                        <Button type="secondary" text="download resume" />
-                    </a>
-                </div>
-            </Section>
+            <AnimatePresence exitBeforeEnter>
+                <Section id="about-me" title="about me">
+                    {
+                        !!aboutPositionFromTop && (isMobile ? aboutPositionFromTop <= 20 : aboutPositionFromTop <= 820) && (
+                            <Fragment>
+                                <motion.p
+                                variants={ sectionVariants }>
+                                    Experienced in full-stack development, with a passion and focus on front-end development. I started my career with an amazing opportunity, after graduating from matric and have grown through all of my experiences and thanks to the help of my seniors. I'm passionate and enjoy the line of work I'm in, always looking to improve and to learn new skills. I work great in teams and I'm perfectly capable to work on my own, I take responsibilities very seriously and make honesty a priority.
+                                </motion.p>
+                                <motion.div
+                                variants={ sectionVariants }
+                                className="section__cta">
+                                    <Link to="/portfolio">
+                                        <Button type="secondary" text="go to portfolio" />
+                                    </Link>
+                                    <a download href={ `${process.env.REACT_APP_FILE_URL}/Corrie_Graham-CV.pdf` } rel="noreferrer" target="_blank">
+                                        <Button type="secondary" text="download resume" />
+                                    </a>
+                                </motion.div>
+                            </Fragment>
+                        )
+                    }
+                </Section>
+
+            </AnimatePresence>
 
             <Section
             id="experience"
@@ -56,7 +70,7 @@ export function HomeView() {
             isVisible={ experiencePositionFromTop ? experiencePositionFromTop <= 824.5 : false }>
                 <AnimatePresence>
                     {
-                       !!experiencePositionFromTop && (isMobile ? experiencePositionFromTop <= 50 : experiencePositionFromTop <= 400)  && (<Timeline data={ experiences }/>)
+                       !!experiencePositionFromTop && (isMobile ? experiencePositionFromTop <= 50 : experiencePositionFromTop <= 800) && (<Timeline data={ experiences }/>)
                     }
                 </AnimatePresence>
             </Section>
