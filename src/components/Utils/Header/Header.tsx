e58a4018bdb45bca803389b6e8956da39178bfe4
Corrie Graham <corriegraham@Corries-MacBook-Air.local>
2021-08-27 14:37:04 +0200

    chore: implemented animations

--- src/components/Utils/Header/Header.tsx
+++ src/components/Utils/Header/Header.tsx
@@ -1,16 +1,34 @@
 import './Header.css';
 import React from 'react';
 import { Button } from '../Button/Button';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface IHeader {
     hasIntro?: boolean;
     intro?: string;
     image?: string;
     className?: string;
+    index?: number;
 }
 
+const headerIntroVariants = {
+    initial: {
+        x: '-100%',
+        opacity: 0,
+    },
+    entry: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            delay: .3,
+            duration: .5,
+            when: 'beforeChildren',
+            staggerChildren: .6
+        }
+    }
+};
+
 /**
- * 
  * @interface IHeader
  * @property { boolean } hasIntro true / false for if the header contains an introduction message
  * @property { string } intro the introduction message
@@ -22,16 +40,26 @@ export function Header(props: IHeader): JSX.Element {
             <div className="header__image">
                 <img src={ props.image } alt="" />
             </div>
+            <AnimatePresence>
             {
-                props.hasIntro ? (
-                    <div className="header__intro">
-                        <h2 className="header__intro--title">{ props.intro }</h2>
-                        <a href="#about-me">
+                props.hasIntro && (
+                    <motion.div
+                    variants={ headerIntroVariants }
+                    initial="initial"
+                    animate="entry"
+                    className="header__intro">
+                        <motion.h2 
+                        variants={ headerIntroVariants }
+                        className="header__intro--title">{ props.intro }</motion.h2>
+                        <motion.a 
+                        variants={ headerIntroVariants }
+                        href="#about-me">
                             <Button text="about me" type="primary"/>
-                        </a>
-                    </div>
-                ) : null
+                        </motion.a>
+                    </motion.div>
+                )
             }
+            </AnimatePresence>
         </header>
     );
 };
