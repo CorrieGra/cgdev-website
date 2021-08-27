b7bfc384560ba86290867299dffe543c9b4540f7
Corrie C. Graham <corrieg@ornico.co.za>
2021-08-27 10:07:05 +0200

    chore: added AnimatePresence

--- src/views/ProjectsView/ProjectsView.tsx
+++ src/views/ProjectsView/ProjectsView.tsx
@@ -5,6 +5,7 @@ import { createTypedHooks } from 'easy-peasy';
 import { IStore } from '../../store';
 import { Card } from '../../components/Card/Card';
 import { useHistory } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 const { useStoreActions, useStoreState } = createTypedHooks<IStore>();
 
@@ -25,9 +26,11 @@ export function ProjectsView() {
 
     return (
         <Fragment>
-            {
-                projects.map((project, index) => (<Card key={ index } id={ project.project_id } project={ project } onClick={() => onClick(project.project_id) } />))
-            }
+            <AnimatePresence>
+                {
+                    projects.map((project, index) => (<Card index={ index } key={ index } id={ project.project_id } project={ project } onClick={() => onClick(project.project_id) } />))
+                }
+            </AnimatePresence>
         </Fragment>
     );
 };
